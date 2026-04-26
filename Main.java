c9f8a707afbefeb477cf3fbf1771f98651ad5a70
AatroxSS <arsenijruslanovic@gmail.com>
2026-04-27 01:19:11 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("========== ХЕШ-ТАБЛИЦЯ (ПРЯМОКУТНИКИ) ==========");
+        System.out.print("Введіть розмір хеш-таблиці: ");
+        int tableSize = scanner.nextInt();
+
+        HashTable hashTable = new HashTable(tableSize);
+
+        System.out.println("\n========== ВСТАВКА ЕЛЕМЕНТІВ ==========");
+
+        hashTable.insert(new Rectangle(0, 10, 5, 0));
+        hashTable.insert(new Rectangle(0, 8, 4, 0));
+        hashTable.insert(new Rectangle(0, 6, 4, 0));
+        hashTable.insert(new Rectangle(0, 12, 6, 0));
+        hashTable.insert(new Rectangle(0, 5, 2, 0));
+        hashTable.insert(new Rectangle(0, 20, 10, 0));
+
+        System.out.println("\n--- Спроба додати елемент, який створить колізію ---");
+        hashTable.insert(new Rectangle(0, 7, 3, 0));
+
+        System.out.println("\n========== РЕЗУЛЬТАТ ==========");
+        hashTable.display();
+
+        scanner.close();
+    }
+}
